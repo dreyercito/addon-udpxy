75d8ba231dcf52e803ce1f118c0149b6048ea93f
Jose Carlos Luna <Jose.Carlos.Luna@cern.ch>
2024-06-02 20:56:01 +0200

    Updated config  for network interface

--- udpxy/run.sh
+++ udpxy/run.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/with-contenv bashio
 declare port
+declare interface
 
 port="5056"
 if bashio::config.has_value "port"; then
     port=$(bashio::config "port")
 fi
 
-#-T ?
+interface="end0"
+if bashio::config.has_value "interface"; then
+    interface=$(bashio::config "interface")
+fi
 
-exec udpxy -m eth0 -p "$port"
+
+#-T ?
+exec udpxy -m "$interface" -p "$port"
 
